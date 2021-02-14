146c3cdaa938303d79d42bd8a3d0a8071d975dc5
Manoj_kesani <mehermanoj45@gmail.com>
2021-02-14 23:07:40 +0530

    Update and rename Tensorboard.py to LR_Call_back_Tensorboard.py

--- LR_Call_back_Tensorboard.py
+++ LR_Call_back_Tensorboard.py
@@ -1,4 +1,13 @@
 from keras.callbacks import TensorBoard,LambdaCallback
+
+class LearningRateLoggingCallback(tf.keras.callbacks.Callback):
+
+    def on_epoch_end(self, epoch):
+        lr = self.model.optimizer._decayed_lr('float32').numpy()
+        tf.summary.scalar('learning rate', data=lr, step=epoch)
+
+
+
 train_lr_callback = LambdaCallback( on_epoch_begin= lambda epoch,logs: print("LearningRate of %e" % (K.eval(resnet20.optimizer._decayed_lr('float32').numpy())) ))
 
 history = resnet20.fit(train_gen,
